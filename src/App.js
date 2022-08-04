3bb437553b38b104050bf2b98da3a3227c596053
Organic-Species <jarrel.thomas@gmail.com>
2022-08-04 14:54:52 -0400

    Contact Page - Finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './components/Home';
+import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact';
 
