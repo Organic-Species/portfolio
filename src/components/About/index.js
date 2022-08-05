2df358535e2d886c8d19a42484f3d2044f579b33
Organic-Species <jarrel.thomas@gmail.com>
2022-08-04 22:02:11 -0400

    About - Animated Letters - Contact: Updated JS

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,41 +1,45 @@
-import './index.scss'
-import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 
 const About = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass} 
-                        strArray={'A', 'b', 'o', 'u', 't', ' ', 'm', 'e'}
-                        idx={15}
-                    />
-                </h1>
-                <p>Excepteur id incididunt magna adipisicing duis velit. Nisi magna cupidatat aute aliqua excepteur pariatur dolore dolor. 
-                Ullamco id cillum sit ullamco cupidatat consectetur elit qui aute exercitation. 
-                Fugiat reprehenderit aliqua voluptate mollit anim anim nisi est ex eiusmod occaecat.
-                </p>
-                <p>Excepteur id incididunt magna adipisicing duis velit. Nisi magna cupidatat aute aliqua excepteur pariatur dolore dolor. 
-                Ullamco id cillum sit ullamco cupidatat consectetur elit qui aute exercitation. 
-                Fugiat reprehenderit aliqua voluptate mollit anim anim nisi est ex eiusmod occaecat.
-                </p>
-                <p>Excepteur id incididunt magna adipisicing duis velit. Nisi magna cupidatat aute aliqua excepteur pariatur dolore dolor. 
-                Ullamco id cillum sit ullamco cupidatat consectetur elit qui aute exercitation. 
-                Fugiat reprehenderit aliqua voluptate mollit anim anim nisi est ex eiusmod occaecat.
-                </p>
-            </div>
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
         </div>
-    )
+        </div>
+    </>
+  )
 }
 
 export default About
